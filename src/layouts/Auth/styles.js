617522bbccc59daeb74defe6e69ea1b28bfa585f
Nick <nicholas.zarate@gmail.com>
2017-07-02 21:22:45 -0600

    Replace LoggedOut with Auth

---
+++ src/layouts/Auth/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20
+  },
+  logo: {
+    textAlign: 'center',
+    marginTop: 200,
+    padding: 20,
+    flex: 6,
+  },
+  input: {
+    height: 40,
+    flex: 2,
+  }
+})
