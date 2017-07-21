a3d0e6df3f1a8bcffade2785fa1caae33347cb90
Nick <nicholas.zarate@gmail.com>
2017-07-21 08:57:17 -0600

    Make Authentication page a stateless functional component

--- src/layouts/Auth/Auth.js
+++ src/layouts/Auth/Auth.js
@@ -4,43 +4,39 @@ import { styles } from './index.js'
 import connect from '../../config/connect'
 import { login } from '../../actions'
 
-class Auth extends React.Component {
-  constructor(props) {
-    super(props)
-    this.handleLogin = this.handleLogin.bind(this)
+const Auth = (props) => {
+  var email
+  var password
+
+  const handleLogin = () => {
+    props.actions.login(email._lastNativeText, password._lastNativeText)
   }
 
-  handleLogin() {
-    this.props.actions.login(this.email._lastNativeText, this.password._lastNativeText)
-  }
-
-  render() {
-    return (
-      <View style={ styles.container }>
-        <Text style={ styles.logo }>{ 'Insert IronWill Logo' }</Text>
-        <Text style={ styles.error }>{ this.props.authentication.loginError }</Text>
-        <TextInput
-          autoCapitalize='none'
-          autoCorrect={ false }
-          keyboardType='email-address'
-          placeholder='email'
-          ref={ (v) => { this.email = v } }
-          returnKeyType='next'
-          style={ styles.input }
-        />
-        <TextInput
-          autoCorrect={ false }
-          placeholder='password'
-          ref={ (v) => { this.password = v } }
-          secureTextEntry={ true }
-          style={ styles.input }
-        />
-        <Button onPress={ this.handleLogin } title='Log In' />
-        <Text>{ 'Forgot Password?' }</Text>
-        <Text>{ 'Don\'t have an account? SIGN UP' }</Text>
-      </View>
-    )
-  }
+  return (
+    <View style={ styles.container }>
+      <Text style={ styles.logo }>{ 'Insert IronWill Logo' }</Text>
+      <Text style={ styles.error }>{ props.authentication.loginError }</Text>
+      <TextInput
+        autoCapitalize='none'
+        autoCorrect={ false }
+        keyboardType='email-address'
+        placeholder='email'
+        ref={ (v) => (email = v) }
+        returnKeyType='next'
+        style={ styles.input }
+      />
+      <TextInput
+        autoCorrect={ false }
+        placeholder='password'
+        ref={ (v) => (password = v) }
+        secureTextEntry={ true }
+        style={ styles.input }
+      />
+      <Button onPress={ handleLogin } title='Log In' />
+      <Text>{ 'Forgot Password?' }</Text>
+      <Text>{ 'Don\'t have an account? SIGN UP' }</Text>
+    </View>
+  )
 }
 
 export default connect({
